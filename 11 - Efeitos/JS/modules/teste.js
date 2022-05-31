9412c6bae96f1226bdd9fc2fc53cb2823add8313
rapha <raphapascon@gmail.com>
2022-05-31 16:23:47 -0300

    Dia 31/05/2022 + 6hrs de aula em 10 horas (ou 12, não lembro)

---
+++ 11 - Efeitos/JS/modules/teste.js
@@ -0,0 +1,11 @@
+
+export function teste3() {
+    console.log('afsfasd')
+}
+
+export function teste1() {
+    console.log('isso é um teste')
+}
+
+
+export const senha = "8978978975ASSBD";
---
+++ 11 - Efeitos/JS/script.js
@@ -0,0 +1,14 @@
+import initScrollSuave from "./modules/scroll-suave.js";
+import initAnimacaoScroll from "./modules/scroll-animacao.js";
+import initAccordion from "./modules/acordion.js";
+import initTabNav from "./modules/tabnav.js";
+import * as teste from "./modules/teste.js";
+
+
+console.log(teste.senha);
+
+initAnimacaoScroll();
+initScrollSuave();
+initAccordion();
+initTabNav();
+teste.teste1();
